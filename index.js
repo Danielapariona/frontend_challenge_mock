114c36557463bbc07654cb923506e4cb3a051fd0
Danielapariona <daniela.parionac@gmail.com>
2021-10-15 17:28:04 -0500

    Update mock

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); //Used to parse JSON bodies
 
+var cors = require('cors');
+app.use(cors());
+
 app.use('/api/campus', Campus);
 const PORT = process.env.PORT || 4000;
 
